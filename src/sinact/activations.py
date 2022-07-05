155ac4edc87ea93f439d2ed7a11b14c4a81dcedc
Jack Maguire <maguirj4@gene.com>
2022-07-05 13:09:38 -0500

    MultipleSineWaveActivation has the right number of parameters

--- src/sinact/activations.py
+++ src/sinact/activations.py
@@ -24,7 +24,7 @@ from tensorflow.python.util.tf_export import keras_export
 @keras_export('keras.layers.SingleSineWaveActivation')
 class SingleSineWaveActivation(Layer):
   def __init__(self,
-               alpha_initializer='zeros',
+               alpha_initializer='glorot_uniform',
                alpha_regularizer=None,
                alpha_constraint=None,
                bias_initializer='zeros',
@@ -94,8 +94,6 @@ class SingleSineWaveActivation(Layer):
         dtype=self.dtype)
 
 
-    print( self._trainable_weights )
-
     # Set input spec
     axes = {}
     if self.shared_axes:
@@ -129,6 +127,92 @@ class SingleSineWaveActivation(Layer):
       'use_bias': self.use_bias,
     }
     base_config = super(SingleSineWaveActivation, self).get_config()
+    return dict(list(base_config.items()) + list(config.items()))
+
+  @tf_utils.shape_type_conversion
+  def compute_output_shape(self, input_shape):
+    return input_shape
+
+@keras_export('keras.layers.MultipleSineWaveActivation')
+class MultipleSineWaveActivation(Layer):
+  def __init__(self,
+               nwaves:int,
+               alpha_initializer='glorot_uniform',
+               alpha_regularizer=None,
+               alpha_constraint=None,
+               bias_initializer='zeros',
+               bias_regularizer=None,
+               bias_constraint=None,
+               shared_axes=None,
+               use_bias=True,
+               **kwargs):
+    super(MultipleSineWaveActivation, self).__init__(**kwargs)
+
+    self.nwaves = nwaves
+
+    self.use_bias = use_bias
+    self.supports_masking = True
+    self.alpha_initializer = alpha_initializer
+    self.alpha_regularizer = alpha_regularizer
+    self.alpha_constraint = alpha_constraint
+    self.bias_initializer = initializers.get(bias_initializer)
+    self.bias_regularizer = regularizers.get(bias_regularizer)
+    self.bias_constraint = constraints.get(bias_constraint)
+    if shared_axes is None:
+      self.shared_axes = None
+    elif not isinstance(shared_axes, (list, tuple)):
+      self.shared_axes = [shared_axes]
+    else:
+      self.shared_axes = list(shared_axes)
+
+  @tf_utils.shape_type_conversion
+  def build(self, input_shape):
+    param_shape = list(input_shape[1:])
+    if self.shared_axes is not None:
+      for i in self.shared_axes:
+        param_shape[i - 1] = 1
+
+    self.waves = []
+    for i in range( 0, self.nwaves ):
+      self.waves.append( SingleSineWaveActivation(alpha_initializer=self.alpha_initializer, alpha_regularizer=self.alpha_regularizer, alpha_constraint=self.alpha_constraint,use_bias=False) )
+
+    if self.use_bias:
+      self.bias = self.add_weight(
+        name='bias',
+        shape=param_shape,
+        initializer=self.bias_initializer,
+        regularizer=self.bias_regularizer,
+        constraint=self.bias_constraint,
+        trainable=True,
+        dtype=self.dtype)
+
+    # Set input spec
+    axes = {}
+    if self.shared_axes:
+      for i in range(1, len(input_shape)):
+        if i not in self.shared_axes:
+          axes[i] = input_shape[i]
+          self.input_spec = InputSpec(ndim=len(input_shape), axes=axes)
+          self.built = True
+
+  def call(self, inputs):
+    y = self.bias
+    for w in self.waves:
+      tf.math.add( y, w(inputs) )
+    return y
+
+  def get_config(self):
+    config = {
+      'alpha_initializer': initializers.serialize(self.alpha_initializer),
+      'alpha_regularizer': regularizers.serialize(self.alpha_regularizer),
+      'alpha_constraint': constraints.serialize(self.alpha_constraint),
+      'bias_initializer': initializers.serialize(self.bias_initializer),
+      'bias_regularizer': regularizers.serialize(self.bias_regularizer),
+      'bias_constraint': constraints.serialize(self.bias_constraint),
+      'shared_axes': self.shared_axes,
+      'use_bias': self.use_bias,
+    }
+    base_config = super(MultipleSineWaveActivation, self).get_config()
     return dict(list(base_config.items()) + list(config.items()))
 
   @tf_utils.shape_type_conversion
--- tests/test1.py
+++ tests/test1.py
@@ -13,7 +13,16 @@ def test1():
     model.compile(optimizer='adam', loss='mean_squared_error')
     print( model.summary() )
 
+def test2( nwaves ):
+    A_in = Input(shape=(5,), name='A_in')
+    out = sa.MultipleSineWaveActivation(nwaves)(A_in)
+    model = Model(inputs=[A_in], outputs=out)
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    print( model.summary() )
 
 
 if __name__ == '__main__':
     test1()
+    test2(1)
+    test2(2)
+    test2(5)
