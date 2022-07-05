018751168ac518229b1028f16151c3fa848020d2
Jack Maguire <maguirj4@gene.com>
2022-07-05 13:01:34 -0500

    use bias

--- src/sinact/activations.py
+++ src/sinact/activations.py
@@ -21,19 +21,27 @@ from tensorflow.python.framework import dtypes
 from tensorflow.python.ops import math_ops
 from tensorflow.python.util.tf_export import keras_export
 
-#@keras_export('keras.layers.SingleSineWaveActivation')
+@keras_export('keras.layers.SingleSineWaveActivation')
 class SingleSineWaveActivation(Layer):
   def __init__(self,
                alpha_initializer='zeros',
                alpha_regularizer=None,
                alpha_constraint=None,
+               bias_initializer='zeros',
+               bias_regularizer=None,
+               bias_constraint=None,
                shared_axes=None,
+               use_bias=True,
                **kwargs):
     super(SingleSineWaveActivation, self).__init__(**kwargs)
+    self.use_bias = use_bias
     self.supports_masking = True
     self.alpha_initializer = initializers.get(alpha_initializer)
     self.alpha_regularizer = regularizers.get(alpha_regularizer)
     self.alpha_constraint = constraints.get(alpha_constraint)
+    self.bias_initializer = initializers.get(bias_initializer)
+    self.bias_regularizer = regularizers.get(bias_regularizer)
+    self.bias_constraint = constraints.get(bias_constraint)
     if shared_axes is None:
       self.shared_axes = None
     elif not isinstance(shared_axes, (list, tuple)):
@@ -75,27 +83,18 @@ class SingleSineWaveActivation(Layer):
       trainable=True
     )
 
+    if self.use_bias:
+      self.bias = self.add_weight(
+        name='bias',
+        shape=param_shape,
+        initializer=self.bias_initializer,
+        regularizer=self.bias_regularizer,
+        constraint=self.bias_constraint,
+        trainable=True,
+        dtype=self.dtype)
+
+
     print( self._trainable_weights )
-
-    '''
-    self.a1 = tf.Variable(
-      initial_value=self.alpha_initializer(shape=param_shape, dtype="float32"),
-      trainable=True,
-      name="a1",
-    )
-
-    self.a2 = tf.Variable(
-      initial_value=self.alpha_initializer(shape=param_shape, dtype="float32"),
-      trainable=True,
-      name="a2",
-    )
-
-    self.a3 = tf.Variable(
-      initial_value=self.alpha_initializer(shape=param_shape, dtype="float32"),
-      trainable=True,
-      name="a3",
-    )
-    '''
 
     # Set input spec
     axes = {}
@@ -114,15 +113,8 @@ class SingleSineWaveActivation(Layer):
         self.alpha3
       ) )
     )
-    '''
-    y = tf.math.multiply(
-      self.a1,
-      tf.math.sin( tf.math.add(
-        (self.a2*inputs),
-        self.a3
-      ) )
-    )
-    '''
+    if self.use_bias:
+      y = tf.math.add( y, self.bias )
     return y
 
   def get_config(self):
@@ -130,104 +122,13 @@ class SingleSineWaveActivation(Layer):
       'alpha_initializer': initializers.serialize(self.alpha_initializer),
       'alpha_regularizer': regularizers.serialize(self.alpha_regularizer),
       'alpha_constraint': constraints.serialize(self.alpha_constraint),
-      'shared_axes': self.shared_axes
+      'bias_initializer': initializers.serialize(self.bias_initializer),
+      'bias_regularizer': regularizers.serialize(self.bias_regularizer),
+      'bias_constraint': constraints.serialize(self.bias_constraint),
+      'shared_axes': self.shared_axes,
+      'use_bias': self.use_bias,
     }
     base_config = super(SingleSineWaveActivation, self).get_config()
-    return dict(list(base_config.items()) + list(config.items()))
-
-  @tf_utils.shape_type_conversion
-  def compute_output_shape(self, input_shape):
-    return input_shape
-
-
-@keras_export('keras.layers.PReLU')
-class PReLU(Layer):
-  """Parametric Rectified Linear Unit.
-
-  It follows:
-
-  ```
-    f(x) = alpha * x for x < 0
-    f(x) = x for x >= 0
-  ```
-
-  where `alpha` is a learned array with the same shape as x.
-
-  Input shape:
-    Arbitrary. Use the keyword argument `input_shape`
-    (tuple of integers, does not include the samples axis)
-    when using this layer as the first layer in a model.
-
-  Output shape:
-    Same shape as the input.
-
-  Args:
-    alpha_initializer: Initializer function for the weights.
-    alpha_regularizer: Regularizer for the weights.
-    alpha_constraint: Constraint for the weights.
-    shared_axes: The axes along which to share learnable
-      parameters for the activation function.
-      For example, if the incoming feature maps
-      are from a 2D convolution
-      with output shape `(batch, height, width, channels)`,
-      and you wish to share parameters across space
-      so that each filter only has one set of parameters,
-      set `shared_axes=[1, 2]`.
-  """
-
-  def __init__(self,
-               alpha_initializer='zeros',
-               alpha_regularizer=None,
-               alpha_constraint=None,
-               shared_axes=None,
-               **kwargs):
-    super(PReLU, self).__init__(**kwargs)
-    self.supports_masking = True
-    self.alpha_initializer = initializers.get(alpha_initializer)
-    self.alpha_regularizer = regularizers.get(alpha_regularizer)
-    self.alpha_constraint = constraints.get(alpha_constraint)
-    if shared_axes is None:
-      self.shared_axes = None
-    elif not isinstance(shared_axes, (list, tuple)):
-      self.shared_axes = [shared_axes]
-    else:
-      self.shared_axes = list(shared_axes)
-
-  @tf_utils.shape_type_conversion
-  def build(self, input_shape):
-    param_shape = list(input_shape[1:])
-    if self.shared_axes is not None:
-      for i in self.shared_axes:
-        param_shape[i - 1] = 1
-    self.alpha = self.add_weight(
-        shape=param_shape,
-        name='alpha',
-        initializer=self.alpha_initializer,
-        regularizer=self.alpha_regularizer,
-        constraint=self.alpha_constraint)
-
-    # Set input spec
-    axes = {}
-    if self.shared_axes:
-      for i in range(1, len(input_shape)):
-        if i not in self.shared_axes:
-          axes[i] = input_shape[i]
-    self.input_spec = InputSpec(ndim=len(input_shape), axes=axes)
-    self.built = True
-
-  def call(self, inputs):
-    pos = backend.relu(inputs)
-    neg = -self.alpha * backend.relu(-inputs)
-    return pos + neg
-
-  def get_config(self):
-    config = {
-        'alpha_initializer': initializers.serialize(self.alpha_initializer),
-        'alpha_regularizer': regularizers.serialize(self.alpha_regularizer),
-        'alpha_constraint': constraints.serialize(self.alpha_constraint),
-        'shared_axes': self.shared_axes
-    }
-    base_config = super(PReLU, self).get_config()
     return dict(list(base_config.items()) + list(config.items()))
 
   @tf_utils.shape_type_conversion
